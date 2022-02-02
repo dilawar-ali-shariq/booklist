d88974f1e6fd2c59e84d3c506e3bcc0f2099e5a7
dilawar ali shariq <dilawarshariq@gmail.com>
2022-02-02 08:31:20 -0800

    third commit

--- app.js
+++ app.js
@@ -1,3 +1,75 @@
+var arrayOfTodo = [];
+var inputBar1 = document.getElementById("input-add");
+var list = document.getElementById("list");
+var wrapper = document.getElementById("wrapper");
+var text = document.getElementById("text");
+var addBtn = document.getElementById("add-btn");
+var backDrop = document.getElementById("back-drop");
+var updateList = document.getElementById("update-list");
+var updateBtn = document.getElementById("update-btn");
+var Index = 0;
+
+addBtn.addEventListener("click", (e) => {
+    if (inputBar1.value != "") {
+        arrayOfTodo.push(inputBar1.value);
+
+    } else {
+        alert("Fill in all fields!");
+    }
+    createUI();
+    inputBar1.value = "";
+})
+
+createUI();
+
+updateBtn.addEventListener("click", (e) => {
+    Update(Index, updateList.value);
+    backDrop.style.display = "none";
+    createUI();
+}) 
+
+function Update(index, value){
+    arrayOfTodo[index] = value;
+}
+
+
+function Delete(index) { 
+    let newArray1 = [];
+    for (let i = 0; i < arrayOfTodo.length; i++) {
+      if ( i != index ) {
+        newArray1.push(arrayOfTodo[i]);
+      }
+    }
+    arrayOfTodo = newArray1;
+}
+
+function createUI() {
+    list.innerHTML = "";
+    for (let i = 0; i < arrayOfTodo.length; i++) {
+        let rawData1 = `<div id="wrapper">
+        <div id="text">${arrayOfTodo[i]}</div>
+        <div class="edit flex" index="${i}"><i class="fas fa-pencil-alt"></i></div>
+        <div class="delete flex" index="${i}"><i class="fas fa-trash-alt"></i></div>
+        </div>`;
+        list.innerHTML = list.innerHTML + rawData1;
+    }
+}
+
+list.addEventListener('click', (e) => {
+    if (e.target.classList.contains("delete")) {
+      let index = +e.target.getAttribute("index");
+      Delete(index);
+      createUI();
+    }
+    if (e.target.classList.contains("edit")) {
+      let index = +e.target.getAttribute("index");
+      Index = +index;
+      backDrop.style.display = "flex";
+      updateList.value = arrayOfTodo[+index];
+      createUI();
+    }
+})
+
 // var arrayOfBookName = [];
 // var arrayOfIsbn = [];;
 // var inputBar1 = document.getElementById("input-book-name");
@@ -89,79 +161,6 @@
 //       createUI();
 //     }
 // })
-
-var arrayOfTodo = [];
-var inputBar1 = document.getElementById("input-add");
-var list = document.getElementById("list");
-var wrapper = document.getElementById("wrapper");
-var text = document.getElementById("text");
-var addBtn = document.getElementById("add-btn");
-var backDrop = document.getElementById("back-drop");
-var updateList = document.getElementById("update-list");
-var updateBtn = document.getElementById("update-btn");
-var Index = 0;
-
-addBtn.addEventListener("click", (e) => {
-    if (inputBar1.value != "") {
-        arrayOfTodo.push(inputBar1.value);
-
-    } else {
-        alert("Fill in all fields!");
-    }
-    createUI();
-    inputBar1.value = "";
-})
-
-createUI();
-
-updateBtn.addEventListener("click", (e) => {
-    Update(Index, updateList.value);
-    backDrop.style.display = "none";
-    createUI();
-}) 
-
-function Update(index, value){
-    arrayOfTodo[index] = value;
-}
-
-
-function Delete(index) { 
-    let newArray1 = [];
-    for (let i = 0; i < arrayOfTodo.length; i++) {
-      if ( i != index ) {
-        newArray1.push(arrayOfTodo[i]);
-      }
-    }
-    arrayOfTodo = newArray1;
-}
-
-function createUI() {
-    list.innerHTML = "";
-    for (let i = 0; i < arrayOfTodo.length; i++) {
-        let rawData1 = `<div id="wrapper">
-        <div id="text">${arrayOfTodo[i]}</div>
-        <div class="edit flex" index="${i}"><i class="fas fa-pencil-alt"></i></div>
-        <div class="delete flex" index="${i}"><i class="fas fa-trash-alt"></i></div>
-        </div>`;
-        list.innerHTML = list.innerHTML + rawData1;
-    }
-}
-
-list.addEventListener('click', (e) => {
-    if (e.target.classList.contains("delete")) {
-      let index = +e.target.getAttribute("index");
-      Delete(index);
-      createUI();
-    }
-    if (e.target.classList.contains("edit")) {
-      let index = +e.target.getAttribute("index");
-      Index = +index;
-      backDrop.style.display = "flex";
-      updateList.value = arrayOfTodo[+index];
-      createUI();
-    }
-})
-
 
 
 
